b7c63c68d7a8f7384c150cb5995f34c68575bb9f
sbx_user1060 <sbx_user1060@ip-10-12-191-168.eu-west-2.compute.internal>
2018-04-23 17:47:59 +0000

    Mon Apr 23 17:47:59 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -5,5 +5,13 @@
 def checkout(skus):
     recognised_skus = 'ABCD'
     counts = {}
+
     for sku in skus:
-        if sku not in
+        if sku in recognised_skus:
+            counts[sku] += 1
+        else:
+            return -1
+
+    for sku in counts:
+        
+
