1eda9b407ff20697c2d5efa490e30de135fc94b8
sbx_user1060 <sbx_user1060@ip-10-12-191-168.eu-west-2.compute.internal>
2018-04-23 18:35:20 +0000

    Mon Apr 23 18:35:20 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -69,42 +69,6 @@ def checkout(skus):
     cost = 0
     for sku in recognised_skus:
         while counts[sku] > 0:
-            if sku == 'A':
-                if counts['A'] / 5 > 0:
-                    counts['A'] -= 5
-                    cost += 200
-                elif counts['A'] / 3 > 0:
-                    counts['A'] -= 3
-                    cost += 130
-                else:
-                    counts[sku] -= 1
-                    cost += 50
-            elif sku == 'B':
-                if counts['B'] / 2 > 0:
-                    counts['B'] -= 2
-                    cost += 45
-                else:
-                    counts['B'] -= 1
-                    cost += 30
-            elif sku == 'C':
-                counts['C'] -= 1
-                cost += 20
-            elif sku == 'D':
-                counts['D'] -= 1
-                cost += 15
-            elif sku == 'E':
-                if counts['B'] > 0 and counts['E'] / 2 > 0:
-                    counts['B'] -= 1
-                    counts['E'] -= 2
-                    cost += 80
-                else:
-                    counts['E'] -= 1
-                    cost += 40
-            elif sku == 'F':
-                if counts['F'] / 3 > 0:
-                    counts['F'] -= 3
-                    cost += 20
-                else:
-                    counts['F'] -= 1
-                    cost += 10
+            if sku in offers_bogof:
+
     return cost
